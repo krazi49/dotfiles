95cca9b5e1715b2b2758cd9a053418013b458b92
kraziest <127786411+krazi49@users.noreply.github.com>
2025-06-20 21:21:13 +0100

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+fastfetch
+
+function fish_greeting
+    random choice "i can't get no sleep" "Ad blockers are not supported in the kitty terminal.
+    In order to continue using kitty, please turn off your ad blocker within the next 3 commands." "shit the fuck up" "shit the fuck up, don't say nun"
+end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
