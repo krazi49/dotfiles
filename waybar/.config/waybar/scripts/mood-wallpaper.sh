83ea88ec7b273d80d6051d9da1e3b21dff47c2ae
kraziest <emmanuelohenhen896@gmail.com>
2026-09-01 19:23:25 +0100

    editing dotfiles in order to save my github calendar

--- waybar/.config/waybar/scripts/mood-wallpaper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Simple mood wallpaper script - prevents service failures
-# Logs mood changes for debugging
-MOOD="$1"
-TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
-echo "[$TIMESTAMP] Mood changed to: $MOOD" >> /tmp/mood-wallpaper.log
-# Actual wallpaper setting would go here
-# For now, just log to prevent service failure
-exit 0
